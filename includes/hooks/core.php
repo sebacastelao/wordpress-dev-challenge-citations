c8627e3ed93053013099e89dab38e96ad86c8690
sebacastelao <sebacastellucci@gmail.com>
2022-08-18 09:23:57 -0300

    Update core.php

--- includes/hooks/core.php
+++ includes/hooks/core.php
@@ -3,3 +3,21 @@
 if ( ! defined('ABSPATH') ) {
     die('Direct access not permitted.');
 }
+
+// Add aditional wp_editor to post
+
+add_action( 'edit_form_after_editor', 'create_wpeditor' );
+add_action( 'save_post', 'save_wp_editor_fields' );
+// add_shortcode('mc-citacion', 'subscribe_link');
+
+/**
+ * Central location to create all shortcodes.
+ */
+
+if (!function_exists('citation_shortcodes_init')) {
+    function citation_shortcodes_init() {
+        add_shortcode( 'citation', 'citation_shortcode' );
+    }
+}
+
+add_action( 'init', 'citation_shortcodes_init' );
